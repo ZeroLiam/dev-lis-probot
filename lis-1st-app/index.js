a5eb45bbc7b9e780aac4e472154567c1cb9a8b0a
ZeroLiam <liselotr@gmail.com>
2018-03-27 06:47:33 +0200

    PUSH

--- lis-1st-app/index.js
+++ lis-1st-app/index.js
@@ -7,13 +7,17 @@ module.exports = (robot) => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-  robot.on(['issues.opened', 'push', 'create'], async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = "Yo"
+  robot.on(['issues.opened', 'issues.reopened'], async context => {
+    //Playing with the issues
+    const resp = context.issue({body: 'Changed the issue status. Let me know if you got a problem with it >:) '});
 
-    // Post a comment on the issue
-    return context.github.issues.createComment(params)
+    return context.github.issues.createComment(resp);
+  });
+
+  robot.on(['push'], async context => {
+    //Playing with the issues
+      const resp = context.issue({number: 9, body: 'WHAT'});
+    return context.github.issues.createComment(resp);
+    // robot.log(context.github.reactions);
   })
 }
